5fb4625d892b7d8b101631365d9a406a90f82ec0
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-07-05 13:50:18 -0500

    Question 2 and 3

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -25,13 +25,16 @@ console.log('A little about me:', me);
   - Console.log fullName
 */
 
-
+const fullName = me.firstName + ' ' + me.lastName;
+console.log(fullName);
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food
 */
 
+console.log(me.favThreeFoods[0]);
+console.log(me.favThreeFoods[me.favThreeFoods.length - 1]);
 
 
 /* 4. Change a property of an existing object.
